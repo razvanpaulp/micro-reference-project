0dd05a68b00652cf29598b97cda02f5647c11b61
razvanpaulp <razvanpaulp@gmail.com>
2018-12-17 10:12:12 +0200

    Encapsulate Undertow in UndertowServer, add websocket handler

--- src/main/java/com/api/resources/ApiResource.java
+++ src/main/java/com/api/resources/ApiResource.java
@@ -25,51 +25,52 @@ import java.util.concurrent.ExecutorService;
 @Path("/")
 @Produces({"application/json", "application/xml"})
 public class ApiResource implements OpenApiInterface {
-	
+
 	@Inject
 	private ApiService service;
 	@Inject
 	private AppExecutors appExecutors;
-	
+
 	@Override
 	@GET
 	@Path("hello/v1")
 	public Response sayHello() {
 		return Response.ok().entity(new Message("Hello there!")).build();
 	}
-	
+
 	@Override
 	@GET
 	@Path("hello/v2")
 	public Message sayPojoHello() {
 		return new Message("Hello again!.");
 	}
-	
+
 
 	@Override
 	@GET
 	@Path("hello/v3")
 	public void sayParametrizedHello(@QueryParam("name") String name, @Suspended AsyncResponse asyncResponse) {
 		ExecutorService executorService = appExecutors.getExecutorService();
-		
+
 		computeAsync(() -> service.buildHelloMessage(name), executorService)
-				.thenApplyAsync(result -> asyncResponse.resume(Response.ok().entity(result).build()), executorService)
-				.exceptionally(error -> asyncResponse.resume(Response.status(400).entity(new Message(error.getMessage())).build()));
+		.thenApplyAsync(result -> asyncResponse.resume(Response.ok().entity(result).build()), executorService)
+		.exceptionally(error -> asyncResponse.resume(Response.status(400).entity(new Message(error.getMessage())).build()));
 	}
-	
+
 	@Override
 	@GET
 	@Path("json")
 	public JsonResponse sayJson() {
 		HashMap<String, String> player = new HashMap<String, String>() {
 			private static final long serialVersionUID = 1L;{
-		    put("Name","Novak");
-		    put("Surname", "Djokovic");
-		}};
-		 return new JsonResponse()
-				   .with("Competition", "WTA")
-	               .with("Rank", 1)
-	               .with("Player", player)
-	               .done();
+				put("Name", "Djokovic");
+				put("Surname","Novak");
+
+			}};
+			return new JsonResponse()
+					.with("Competition", "ATP")
+					.with("Rank", 1)
+					.with("Player", player)
+					.done();
 	}
 }
