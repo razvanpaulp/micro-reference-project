485d51b55643a5a9fa6eea942f4da21840b4a509
razvanpaulp <razvanpaulp@gmail.com>
2019-01-07 09:32:44 +0200

    Basic auth

---
+++ src/main/java/com/undertow/standalone/MapIdentityManager.java
@@ -0,0 +1,97 @@
+package com.undertow.standalone;
+
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.Credential;
+import io.undertow.security.idm.DigestCredential;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.security.idm.PasswordCredential;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A simple {@link IdentityManager} implementation, that just takes a map of users to their
+ * password.
+ *
+ * This is in now way suitable for real world production use.
+ *
+ *
+*/
+class MapIdentityManager implements IdentityManager {
+
+    private final Map<String, char[]> users;
+
+    MapIdentityManager(final Map<String, char[]> users) {
+        this.users = users;
+    }
+
+    @Override
+    public Account verify(Account account) {
+        // An existing account so for testing assume still valid.
+        return account;
+    }
+
+    @Override
+    public Account verify(String id, Credential credential) {
+        Account account = getAccount(id);
+        if (account != null && verifyCredential(account, credential)) {
+            return account;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Account verify(Credential credential) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private boolean verifyCredential(Account account, Credential credential) {
+        if (credential instanceof PasswordCredential) {
+            char[] password = ((PasswordCredential) credential).getPassword();
+            char[] expectedPassword = users.get(account.getPrincipal().getName());
+
+            return Arrays.equals(password, expectedPassword);
+        }
+        
+        if (credential instanceof DigestCredential) {
+        	 char[] expectedPassword = users.get(account.getPrincipal().getName());
+            return ((DigestCredential) credential).verifyHA1(expectedPassword.toString().getBytes());
+
+        }
+        
+        return false;
+    }
+
+    private Account getAccount(final String id) {
+        if (users.containsKey(id)) {
+            return new Account() {
+
+                private final Principal principal = new Principal() {
+
+                    @Override
+                    public String getName() {
+                        return id;
+                    }
+                };
+
+                @Override
+                public Principal getPrincipal() {
+                    return principal;
+                }
+
+                @Override
+                public Set<String> getRoles() {
+                    return Collections.emptySet();
+                }
+
+            };
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/undertow/standalone/UndertowServer.java
+++ src/main/java/com/undertow/standalone/UndertowServer.java
@@ -5,6 +5,10 @@ import static io.undertow.servlet.Servlets.deployment;
 import static io.undertow.servlet.Servlets.listener;
 import static io.undertow.servlet.Servlets.servlet;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -19,6 +23,18 @@ import com.server.Server;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
+import io.undertow.security.api.AuthenticationMechanism;
+import io.undertow.security.api.AuthenticationMode;
+import io.undertow.security.handlers.AuthenticationCallHandler;
+import io.undertow.security.handlers.AuthenticationConstraintHandler;
+import io.undertow.security.handlers.AuthenticationMechanismsHandler;
+import io.undertow.security.handlers.SecurityInitialHandler;
+import io.undertow.security.idm.DigestAlgorithm;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.security.impl.BasicAuthenticationMechanism;
+import io.undertow.security.impl.DigestAuthenticationMechanism;
+import io.undertow.security.impl.DigestQop;
+import io.undertow.security.impl.SimpleNonceManager;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.GracefulShutdownHandler;
 import io.undertow.server.handlers.PathHandler;
@@ -40,11 +56,11 @@ import static io.undertow.Handlers.websocket;
 public final class UndertowServer {
 
 	public final Lock LOCK = new ReentrantLock();
-	
+
 	private final String host;
 	private final int port;
 	private final String deploymentName;
-	
+
 	private volatile Undertow server;
 
 	public UndertowServer(String host, int port, String deploymentName){
@@ -62,7 +78,7 @@ public final class UndertowServer {
 				.addWelcomePage("index.html")
 				.setDeploymentName(deploymentName)
 				.addServlets(
-				servlet("jerseyServlet", ServletContainer.class)
+						servlet("jerseyServlet", ServletContainer.class)
 						.addInitParam("javax.ws.rs.Application", ApplicationConfig.class.getName())
 						.addMapping("/api/*")
 						.setLoadOnStartup(1)
@@ -110,16 +126,35 @@ public final class UndertowServer {
 
 
 	public void start() throws ServletException{
-		
+
 		final HttpHandler httpHandler = bootstrap();
 		final StuckThreadDetectionHandler stuck = new StuckThreadDetectionHandler(getProperty("thread.execution.time", 100), httpHandler);
 		final GracefulShutdownHandler shutdown = Handlers.gracefulShutdown(stuck);
 
+
+
+		final Map<String, char[]> users = new HashMap<>(2);
+		users.put("userOne", "passwordOne".toCharArray());
+		users.put("userTwo", "passwordTwo".toCharArray());
+
+		final IdentityManager identityManager = new MapIdentityManager(users);
+		HttpHandler handler = shutdown;
+		handler = new AuthenticationCallHandler(handler);
+		handler = new AuthenticationConstraintHandler(handler);
+		List<DigestAlgorithm> digestAlgorithms = Collections.singletonList(DigestAlgorithm.MD5);
+        List<DigestQop> digestQops = Collections.emptyList();
+   
+		final List<AuthenticationMechanism> mechanisms = Collections.<AuthenticationMechanism>singletonList(new BasicAuthenticationMechanism( "My Realm"));
+		
+		//final List<AuthenticationMechanism> mechanisms = Collections.<AuthenticationMechanism>singletonList(new DigestAuthenticationMechanism(digestAlgorithms, digestQops, "My Realm", "/",
+                //new SimpleNonceManager()));
+		handler = new AuthenticationMechanismsHandler(handler, mechanisms);
+		handler = new SecurityInitialHandler(AuthenticationMode.PRO_ACTIVE, identityManager, handler);
 		LOCK.lock();
 
 		server = Undertow.builder()
 				.addHttpListener(port, host)
-				.setHandler(shutdown)
+				.setHandler(handler)
 				.setServerOption(UndertowOptions.ENABLE_HTTP2, true)
 				.build();
 
@@ -129,7 +164,7 @@ public final class UndertowServer {
 
 
 	public void stop() {
-		
+
 		server.stop();
 		LOCK.unlock();
 	}
